cfd1f51f328c7b0c5736de06ca29cccfe3a3a967
Michiyo <73595161+jam3748@users.noreply.github.com>
2021-10-12 23:35:16 +0900

    DEBUGをTrueに変更

--- sample20210826/settings.py
+++ sample20210826/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-55ylo5j$2*h3)9^-llehqkyoz@2kvy6w)i7z*e-_8m135eyp+8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["polar-lake-66430.herokuapp.com", "127.0.0.1"]
 
